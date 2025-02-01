8df4ce54c24eda423fe1ccd0cc0b8b71f4208cc4
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-01 19:58:53 +0330

    create function for get all product details with react query

--- client/src/featurs/products/useProductDetails.js
+++ client/src/featurs/products/useProductDetails.js
@@ -1,5 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
+import { useAddCart } from "../../context/ShopingContext";
+import { getProductDetails } from "../../services/apiProducts";
 
 export function useProductDetails() {
-  const {} = useQuery({});
+  const { addCart } = useAddCart();
+  const { productDetails, isLoading } = useQuery({
+    queryKey: ["products"],
+    queryFn: () => getProductDetails(addCart),
+  });
+  return { productDetails, isLoading };
 }
