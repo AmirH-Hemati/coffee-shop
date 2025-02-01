abf60a3b7be99e6f8898002c63f53bd5e909539f
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-01 22:52:13 +0330

    create api for get inforamation user

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,8 @@
 import express from "express";
+import { me } from "../contorollers/user.js";
+import { auth } from "../middelware/auth.js";
 
 const router = express.Router();
-router.get("/me", me);
+router.get("/me", auth, me);
 
 export default router;
