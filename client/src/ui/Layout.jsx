5af7e55eb133bb0d1f79eafeb1843d5a3f441efd
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-03 15:21:00 +0330

    delete unused conponent

--- client/src/ui/Layout.jsx
+++ client/src/ui/Layout.jsx
@@ -1,12 +1,9 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
-import MenuMobile from "./MenuMobile";
-
 function Layout() {
   return (
     <div className="flex flex-col h-screen w-full overflow-hidden">
       <Header />
-      {/* <MenuMobile /> */}
       <main className=" flex-1 bg-[#F9F5F5]">
         <Outlet />
       </main>
