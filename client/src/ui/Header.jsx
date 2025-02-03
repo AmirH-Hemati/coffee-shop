1c3ef547c4f7ca784498e3da48dd9969bfb69251
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-03 23:45:29 +0330

    add logout in button for logout account

--- client/src/ui/Header.jsx
+++ client/src/ui/Header.jsx
@@ -17,11 +17,11 @@ import { useUser } from "../featurs/authorizaion/useUser";
 import Avatar from "./Avatar";
 import Sidebar from "./Sidebar";
 import { NavLink } from "react-router-dom";
+import Button from "./Button";
 function Header() {
-  const { getTotalQty } = useAddCart();
-  const { token, role } = useAuth();
   const [isOpen, setIsOpen] = useState(false);
-  const { user } = useUser();
+  const { token, role } = useAuth();
+  const { getTotalQty } = useAddCart();
   return (
     <header className="fixed z-50 md:relative bottom-0 bg-white left-0 w-full flex md:flex-row-reverse justify-between h-16  border-b border-b-gray-600  items-center p-4 gap-5">
       <div className="flex gap-6 md:flex-row-reverse w-1/2  justify-around md:justify-start ">
@@ -33,12 +33,6 @@ function Header() {
           <Heart size="32" color="#FF8A65" className="md:hidden" />
           <p className="hidden md:block">Favoriets</p>
         </NavLink>
-        {token && role === "admin" && (
-          <NavLink to="/adminDashboard">
-            <UserEdit size="32" color="#FF8A65" className="md:hidden" />
-            <p className="hidden md:block">PanelAdmin</p>
-          </NavLink>
-        )}
       </div>
       <div className="flex gap-4 items-center flex-row-reverse md:flex-row w-1/2 justify-around md:justify-start">
         {token ? (
@@ -49,13 +43,13 @@ function Header() {
             <LoginCurve size="32" color="#FF8A65" className="md:hidden" />
           </NavLink>
         )}
-        <NavLink to="/cart" classNameStyle="">
-          {/* {getTotalQty() > 0 && (
-    <Button style={`absolute left-2 top-2`} typeButton="circleButton">
-      {getTotalQty()}
-    </Button>
-  )} */}
+        <NavLink to="/cart" className={`relative`}>
           <ShoppingCart size="32" color="black" />
+          {getTotalQty() > 0 && (
+            <Button style={`absolute -left-2 -top-2`} typeButton="circleButton">
+              {getTotalQty()}
+            </Button>
+          )}
         </NavLink>
       </div>
       <User setIsOpen={setIsOpen} isOpen={isOpen} />
@@ -65,6 +59,7 @@ function Header() {
 
 function User({ setIsOpen, isOpen }) {
   const { user } = useUser();
+  const { token, role, logout } = useAuth();
   return (
     <div
       className={` md:rounded-t-2xl  flex flex-col gap-8 bg-white p-8 w-full md:w-1/4 h-full top-0  z-50 fixed -left-full transition-all duration-500 [box-shadow:4px_0px_2px_rgba(0,0,0,0.1)] ${
@@ -83,6 +78,19 @@ function User({ setIsOpen, isOpen }) {
           className="cursor-pointer"
         />
       </div>
+      {token && role === "admin" && (
+        <NavLink
+          to="/adminDashboard"
+          className={({ isActive }) =>
+            `flex hover:bg-[#F9FAFB] p-3 rounded-sm items-center gap-3 ${
+              isActive ? "bg-[#F9FAFB]" : "bg-white"
+            }`
+          }
+        >
+          <UserEdit size="32" color="#00512C" />
+          <p>PanelAdmin</p>
+        </NavLink>
+      )}
       <Sidebar
         onClick={() => setIsOpen(false)}
         data={[
@@ -105,7 +113,7 @@ function User({ setIsOpen, isOpen }) {
       />
       <div className="flex hover:bg-[#F9FAFB] p-3 rounded-sm items-center gap-3  cursor-pointer">
         <LogoutCurve size="32" color="#00512C" />
-        <p>Sign Out</p>
+        <p onClick={logout}>Sign Out</p>
       </div>
     </div>
   );
